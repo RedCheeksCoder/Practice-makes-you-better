3b6183fbf872f3cbff4ef514aa9d012ceaa1c7fb
John Bryan Odina <johnbryanodina.dev@gmail.com>
2024-01-26 23:18:08 +0800

    Edit main.py

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-greet = "Hello world!"
-print(type(greet))
+#greet = "Hello world!"
+#print(type(greet))
+
+#first_name = "Bryan"
+#last_name = "Odina"
+#full_name = first_name + " " + last_name
+
+#print("Hello " + full_name)
+
+#INTEGER
+
+#age = 21
+#age += 1
+
+#print(age)
+#print(type(age))
+
+isStudent = True
+print("Are you a student?: " + str(isStudent))
