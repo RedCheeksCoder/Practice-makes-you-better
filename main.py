2a36943c8fb9cb3a7afeb35b2e79186b1f09d693
John Bryan Odina <johnbryanodina.dev@gmail.com>
2024-01-27 01:36:28 +0800

    Until bmi calculator

--- main.py
+++ main.py
@@ -15,5 +15,76 @@
 #print(age)
 #print(type(age))
 
-isStudent = True
-print("Are you a student?: " + str(isStudent))
+
+#isStudent = True
+#print("Are you a student?: " + str(isStudent))
+
+
+
+#MULTIPLE ASSIGNMENT
+#Multiple variable assignment
+
+
+""" is_student = True
+from_taguig = False
+stem_student = True
+
+
+
+gpa = 3.5
+grades = 2.75
+height = 167.22
+
+
+user_name = input("Enter your name: ")
+
+print("Hello, " + user_name + "!")
+
+
+#snake_case
+student_name = "beatrice"
+average_grade = 85
+class_schedule = "Saturdays and Mondays"
+
+#camelCase
+studentName = "beatrice"
+averageGrade = 85
+classSchedule = "Saturdays and Mondays"
+
+age = 15
+number_of_apples = 23
+subjects = 7
+
+name = "John"
+button = "Green"
+coffee = "Is good" """
+
+
+""" name = "Bryan"
+school = "TUP-Taguig"
+age = 20
+height = 167.2
+weight = 70
+grades = 2.25
+from_taguig = True;
+
+print(f"Hi Im {name}, from {school}. And I am {age} with the height of {height} and weight of {weight}. Recently I got my grades and It's {grades}. Are you from TUP-Taguig too? {from_taguig}") """
+
+
+""" user_name = input("Enter your name: ")
+
+print(f"Hello, {user_name}") """
+
+""" #BMI CALCULATOR
+# Get user input
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height_meters = float(input("Enter your height in meters: "))
+weight_kg = float(input("Enter your weight in kilograms: "))
+
+bmi = weight_kg / (height_meters ** 2)
+
+print(f"Name: {name}")
+print(f"Age: {age} years")
+print(f"BMI: {bmi:.2f}") """
+
